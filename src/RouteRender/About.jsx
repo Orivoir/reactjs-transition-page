cc002a59b3a07135d55d29a746a5fc461585baab
S.Gaborieau <sam.gabor@hotmail.com>
2019-08-20 15:06:27 +0200

    CRA Initial commit

---
+++ src/RouteRender/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Home extends React.Component {
+    
+    render() {
+
+        return (
+            <section className="About">
+
+            </section>
+        )
+    }
+};
