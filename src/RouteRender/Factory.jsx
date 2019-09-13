c7d201db50c337b3d20607a33e91ca70feaef369
S.Gaborieau <sam.gabor@hotmail.com>
2019-09-13 11:21:29 +0200

    add factory section in build

--- src/RouteRender/Factory.jsx
+++ src/RouteRender/Factory.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import DocumentTitle from 'react-document-title';
+import './Factory.css';
+import flaskSvg from './flask.svg';
+import worksWeb from './worksWeb.png'
 
 export default class Home extends React.Component {
     
@@ -12,6 +15,34 @@ export default class Home extends React.Component {
                         <span>Lorem Senzum</span>
                         <blockquote>Factory template of <a data-tip="go 2 real platform of Senzu" href="https://senzu.app">senzu.app</a></blockquote>
                     </h1>
+
+                    <section className="build">
+
+                        <h2>
+                            Cette section en cours de construction
+                            <figure>
+                                <img
+                                    src={flaskSvg}
+                                    alt="fiole chimie caricature animé"
+                                    width="55"
+                                    height="55"
+                                />
+                            </figure>
+                        </h2>
+
+                        <section>
+                            <figure>
+                                <img
+                                    className="worksWeb"
+                                    src={worksWeb}
+                                    alt="fiole chimie caricature animé"
+                                />
+                            </figure>
+
+                        </section>
+
+                    </section>
+
                 </section>
             </DocumentTitle>
         )
